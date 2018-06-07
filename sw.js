bc594f0e5581efdf7d99d174f6dc925dde0460d9
Chad <andchadtx@gmail.com>
2018-06-07 13:42:31 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
-const version = "1.0.1";
-const staticCacheName = `mws-restaurant-${version}`;
+let CACHE_NAME = `mws-restaurant-1`;
 
 self.addEventListener("install", event => {
     event.waitUntil(
         caches
-            .open(staticCacheName)
+            .open(CACHE_NAME)
             .then(cache =>
                 cache
                     .addAll([
@@ -17,6 +16,7 @@ self.addEventListener("install", event => {
                         "./js/main.js",
                         "./js/dbhelper.js",
                         "./js/restaurant_info.js",
+                        "./data/restaurants.json",
                         "./img/1.jpg",
                         "./img/2.jpg",
                         "./img/3.jpg",
@@ -42,88 +42,21 @@ self.addEventListener("activate", event => {
                     cacheNames
                         .filter(
                             cacheName =>
-                                cacheName.startsWith("mws-") && cacheName != staticCacheName
+                                cacheName.startsWith("mws-") && cacheName != CACHE_NAME
                         )
                         .map(cacheName => caches.delete(cacheName))
                 )
             )
     );
 });
-
 self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request).then(
             response =>
                 response ||
                 fetch(event.request).then(response =>
-                    caches.open(staticCacheName).then(cache => {
+                    caches.open(CACHE_NAME).then(cache => {
                         cache.put(event.request.url, response.clone());
-
-                        return response;
-                    })
-                )
-        )
-    );
-});onst version = "1.0.1";
-const staticCacheName = `mws-restaurant-${version}`;
-
-self.addEventListener("install", e => {
-    event.waitUntil(
-        caches
-            .open(staticCacheName)
-            .then(cache =>
-                cache
-                    .addAll([
-                        "./",
-                        "./index.html",
-                        "./restaurant.html",
-                        "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.css",
-                        "./css/styles.css",
-                        "./css/styles-min.css",
-                        "./js/main.js",
-                        "./js/dbhelper.js",
-                        "./js/restaurant_info.js",
-                        "./img/1.jpg",
-                        "./img/2.jpg",
-                        "./img/3.jpg",
-                        "./img/4.jpg",
-                        "./img/5.jpg",
-                        "./img/6.jpg",
-                        "./img/7.jpg",
-                        "./img/8.jpg",
-                        "./img/9.jpg",
-                        "./img/10.jpg"
-                    ])
-                    .then(() => self.skipWaiting())
-            )
-    );
-});
-
-self.addEventListener("activate", e => {
-    event.waitUntil(
-        caches
-            .keys()
-            .then(cacheNames =>
-                Promise.all(
-                    cacheNames
-                        .filter(
-                            cacheName =>
-                                cacheName.startsWith("mws-") && cacheName != staticCacheName
-                        )
-                        .map(cacheName => caches.delete(cacheName))
-                )
-            )
-    );
-});
-
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(e.request).then(
-            response =>
-                response ||
-                fetch(event.request).then(response =>
-                    caches.open(staticCacheName).then(cache => {
-                        cache.put(e.request.url, response.clone());
 
                         return response;
                     })
