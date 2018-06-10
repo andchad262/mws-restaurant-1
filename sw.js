9db8c249a6103e6234905c11c0f228040a2ebac4
Chad <andchadtx@gmail.com>
2018-06-10 11:44:47 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,66 +1,63 @@
-let CACHE_NAME = 'mws-restaurant-1';
+var staticCacheName = "restaurant-static-v2";
+self.addEventListener("install", function (event) {
+    var urlsToCache = [
+        '/',
+        '/index.html',
+        '/restaurant.html',
+        '/css/styles.css',
+        '/data/restaurants.json',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        './img/1.jpg',
+        './img/2.jpg',
+        './img/3.jpg',
+        './img/4.jpg',
+        './img/5.jpg',
+        './img/6.jpg',
+        './img/7.jpg',
+        './img/8.jpg',
+        './img/9.jpg',
+        './img/10.jpg',
+    ]
 
-self.addEventListener("install", event => {
     event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(cache =>
-                cache
-                    .addAll([
-                        "./",
-                        "./index.html",
-                        "./restaurant.html",
-                        "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.css",
-                        "./css/styles.css",
-                        "./css/styles-min.css",
-                        "./js/main.js",
-                        "./js/dbhelper.js",
-                        "./js/restaurant_info.js",
-                        "./data/restaurants.json",
-                        "./img/1.jpg",
-                        "./img/2.jpg",
-                        "./img/3.jpg",
-                        "./img/4.jpg",
-                        "./img/5.jpg",
-                        "./img/6.jpg",
-                        "./img/7.jpg",
-                        "./img/8.jpg",
-                        "./img/9.jpg",
-                        "./img/10.jpg"
-                    ])
-                    .then(() => self.skipWaiting())
-            )
-    );
+        caches.open(staticCacheName).then(function (cache) {
+            console.log("Cache Successful");
+            return cache.addAll(urlsToCache);
+        })
+    )
+})
+
+self.addEventListener('fetch', function (e) {
+    if (e.pathname === '/') {
+        e.respondWith(
+            caches.open(dataCacheName).then(function (cache) {
+                return fetch(e.request).then(function (response) {
+                    cache.put(e.request.url, response.clone());
+                    return response;
+                });
+            })
+        );
+    } else {
+        e.respondWith(
+            caches.match(e.request).then(function (response) {
+                return response || fetch(e.request);
+            })
+        );
+    }
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener('activate', function (event) {
     event.waitUntil(
-        caches
-            .keys()
-            .then(cacheNames =>
-                Promise.all(
-                    cacheNames
-                        .filter(
-                            cacheName =>
-                                cacheName.startsWith("mws-") && cacheName != CACHE_NAME
-                        )
-                        .map(cacheName => caches.delete(cacheName))
-                )
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    return cacheName.startsWith('restaurant-') && cacheName !== staticCacheName;
+                }).map(function (cacheName) {
+                    return caches.delete(cacheName)
+                })
             )
-    );
-});
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(
-            response =>
-                response ||
-                fetch(event.request).then(response =>
-                    caches.open(CACHE_NAME).then(cache => {
-                        cache.put(event.request.url, response.clone());
-
-                        return response;
-                    })
-                )
-        )
-    );
-});
+        })
+    )
+})
